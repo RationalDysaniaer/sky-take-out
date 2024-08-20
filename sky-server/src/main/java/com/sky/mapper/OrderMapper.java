f4590f4f5be9155d34b5ff2dd325ca256598cc85
RationalDysaniaer <122082502+RationalDysaniaer@users.noreply.github.com>
2024-08-20 12:49:16 +0800

    跳过支付

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -24,4 +24,15 @@ public interface OrderMapper {
      * @param orders
      */
     void update(Orders orders);
+
+    /**
+     * (跳过支付使用)
+     * 根据订单号和用户id查询订单
+     * @param orderNumber
+     * @param userId
+     * @return
+     */
+    @Select("select * from orders where number = #{orderNumber} and user_id= #{userId}")
+    Orders getByNumberAndUserId(String orderNumber, Long userId);
+
 }
